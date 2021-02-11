0be81598a6e9acc92df2b6164209d1e874c8f615
José Lucas <jltorquato12@gmail.com>
2021-02-11 14:12:41 -0300

    Criado a LICENSE melhorado o README e corrigido alguns bugs

--- runner/runner.py
+++ runner/runner.py
@@ -4,8 +4,7 @@ import os
 print('No seu navegador digite => localhost:3000')
 print('Servidor rodando...')
 print()
-# linha de comando
+# linha de comando localmente
 os.chdir('/home/jose/vscode/teste_mconf/api_go/api')
-# container docker ?
-# os.chdir('/go/src')
+
 os.system('go run server.go')
